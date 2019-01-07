85eeb90f6e9289c4eccc91725d8d930b2f3420d9
gesco <gescobedoz@gmail.com>
2019-01-07 13:31:48 -0600

    RESTful API, GET and POST Data

--- contactlistapp/public/controllers/controller.js
+++ contactlistapp/public/controllers/controller.js
@@ -1,11 +1,23 @@
 function AppCtrl($scope, $http) {
   console.log("Hello World from controller");
 
-$http.get('/contactlist').success(function(response) {
-  console.log("I got the data I requested");
-  $scope.contactlist = response;
-});
+var refresh = function() {
+  $http.get('/contactlist').success(function(response) {
+    console.log("I got the data I requested");
+    $scope.contactlist = response;
+    $scope.contact = "";
+  });
+};
 
+refresh();
+
+$scope.addContact = function() {
+  console.log($scope.contact);
+  $http.post('/contactlist', $scope.contact).success(function(response) {
+    console.log(response);
+    refresh();
+  });
+};
 
 
 }
--- contactlistapp/server.js
+++ contactlistapp/server.js
@@ -1,31 +1,27 @@
 var express = require('express');
 var app = express();
+var mongojs = require('mongojs');
+var db = mongojs('contactlist', ['contactlist']);
+var bodyParser = require('body-parser');
 
 app.use(express.static(__dirname + "/public"));
+app.use(bodyParser.json());
 
 app.get('/contactlist', function (req, res) {
   console.log("I received a GET request");
 
-  person1 = {
-    name: 'Tim',
-    email: 'tim@email.com',
-    number: '111-111-1111'
-  };
+  db.contactlist.find(function (err, docs) {
+    console.log(docs);
+    res.json(docs);
+  });
 
-  person2 = {
-    name: 'Emily',
-    email: 'emily@email.com',
-    number: '222-222-2222'
-  };
+});
 
-  person3 = {
-    name: 'John',
-    email: 'john@email.com',
-    number: '333-333-3333'
-  };
-
-  var contactlist = [person1, person2, person3];
-  res.json(contactlist);
+app.post('/contactlist', function (req, res) {
+  console.log(req.body);
+  db.contactlist.insert(req.body, function(err, doc) {
+    res.json(doc);
+  });
 });
 
 app.listen(3000);
