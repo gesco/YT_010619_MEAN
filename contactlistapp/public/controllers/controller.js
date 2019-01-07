69292af98da16dcc734992988730bcc80b5c1b86
gesco <gescobedoz@gmail.com>
2019-01-07 01:47:39 -0600

    AngularJS and NodeJS App

--- contactlistapp/public/controllers/controller.js
+++ contactlistapp/public/controllers/controller.js
@@ -1,3 +1,11 @@
-function AppCtrl() {
+function AppCtrl($scope, $http) {
   console.log("Hello World from controller");
+
+$http.get('/contactlist').success(function(response) {
+  console.log("I got the data I requested");
+  $scope.contactlist = response;
+});
+
+
+
 }
--- contactlistapp/server.js
+++ contactlistapp/server.js
@@ -3,5 +3,30 @@ var app = express();
 
 app.use(express.static(__dirname + "/public"));
 
+app.get('/contactlist', function (req, res) {
+  console.log("I received a GET request");
+
+  person1 = {
+    name: 'Tim',
+    email: 'tim@email.com',
+    number: '111-111-1111'
+  };
+
+  person2 = {
+    name: 'Emily',
+    email: 'emily@email.com',
+    number: '222-222-2222'
+  };
+
+  person3 = {
+    name: 'John',
+    email: 'john@email.com',
+    number: '333-333-3333'
+  };
+
+  var contactlist = [person1, person2, person3];
+  res.json(contactlist);
+});
+
 app.listen(3000);
 console.log("Server running on port 3000");
